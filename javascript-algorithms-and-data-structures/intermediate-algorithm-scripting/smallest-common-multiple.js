47e5a05202ec36532498bb5f7a04cf0650716a42
Andrew Steffey <asteffey@gmail.com>
2019-01-15 00:13:27 -0500

    Refactor smallest-common-multiple

--- javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/smallest-common-multiple.js
+++ javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/smallest-common-multiple.js
@@ -1,51 +1,58 @@
 function getPrimesUnderLimit(limit) {
-  let primeCandidate = new Array(limit+1);
+  let primeCandidate = new Array(limit + 1);
   primeCandidate.fill(false, 0, 2);
-  primeCandidate.fill(true, 2, limit+1);
-  
-  for (let i=2; i<=Math.floor(Math.sqrt(limit)); i++) {
-  if (primeCandidate[i])
-      for (let j=Math.pow(i,2); j<=limit; j+=i)
+  primeCandidate.fill(true, 2, limit + 1);
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(limit)); i++) {
+    if (primeCandidate[i])
+      for (let j = Math.pow(i, 2); j <= limit; j += i)
         primeCandidate[j] = false;
   }
-  
+
   return primeCandidate
-          .map((value, index) => value?index:value)
-          .filter(value => value!=false);
+    .map((value, index) => value ? index : value)
+    .filter(value => value != false);
+}
+
+function findTimesDivisible(num, divisor) {
+  let timesDivisible = 0;
+  while (num % divisor == 0) {
+    timesDivisible += 1;
+    num /= divisor;
+  }
+  return timesDivisible;
+}
+
+function reduceToProduct(product, value) {
+  return product * value;
+}
+
+function findLeastCommonMultiple(...args) {
+  let maxExponentOfEachPrime = {};
+  getPrimesUnderLimit(Math.max(...args)).forEach(prime => {
+    maxExponentOfEachPrime[prime] = 0;
+  });
+
+  for (let numToFactorize of args)
+    for (let prime in maxExponentOfEachPrime)
+      maxExponentOfEachPrime[prime] = Math.max(maxExponentOfEachPrime[prime], findTimesDivisible(numToFactorize, prime));
+
+  return Object.keys(maxExponentOfEachPrime).map(prime => Math.pow(prime, maxExponentOfEachPrime[prime]))
+    .reduce(reduceToProduct, 1);
+}
+
+function createNumberSequence(min, max) {
+  let sequence = [];
+  for (let n = min; n <= max; n++)
+    sequence.push(n);
+  return sequence;
 }
 
 function smallestCommons(inclusiveRangeEndpoints) {
-  let rangeMin = Math.max(2, Math.min(...inclusiveRangeEndpoints));
-  let rangeMax = Math.max(...inclusiveRangeEndpoints);
+  let min = Math.min(...inclusiveRangeEndpoints);
+  let max = Math.max(...inclusiveRangeEndpoints);
 
-  let primes = getPrimesUnderLimit(rangeMax);
-  console.log(primes);
-  let exponentOfEachPrime = new Array(primes.length).fill(0);
-  
-  for (let i=rangeMin; i<=rangeMax; i++) {
-    let numToFactorize = i;
-    for (let j=0; j<primes.length; j++) {
-      let prime = primes[j];
-      let exponentOfPrime = 0;
-      while (numToFactorize%prime == 0) {
-        exponentOfPrime += 1;
-        numToFactorize /= prime;
-        //console.log(i + " % " + prime + "^" + exponentOfPrime + " == 0");
-      }
-      exponentOfEachPrime[j] = Math.max(exponentOfEachPrime[j], exponentOfPrime);
-    }
-  }
-
-  console.log(exponentOfEachPrime);
-
-  let smallestCommonMultiple = 1;
-  for (let i=0; i<primes.length; i++) {
-    //console.log(primes[i]+"^"+exponentOfEachPrime[i]);
-    smallestCommonMultiple *= Math.pow(primes[i], exponentOfEachPrime[i]);
-  }
-    
-
-  return smallestCommonMultiple;
+  return findLeastCommonMultiple(...createNumberSequence(min, max));
 }
 
-console.log(smallestCommons([1,5]));
+console.log(smallestCommons([1, 5]));
