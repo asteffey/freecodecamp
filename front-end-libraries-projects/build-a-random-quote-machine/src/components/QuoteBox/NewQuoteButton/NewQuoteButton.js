da6a4952bf1d3e659ea3f334ff283bba41436d38
=Andrew Steffey <asteffey@gmail.com>
2019-08-17 21:15:42 -0400

    Simplify NewQuoteButton with hook

--- front-end-libraries-projects/build-a-random-quote-machine/src/components/QuoteBox/NewQuoteButton/NewQuoteButton.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-export default ({ isLoading, nextId }) => (
-    <NavLink id="new-quote" to={`/${nextId}`}>New Quote</NavLink>
-);
--- front-end-libraries-projects/build-a-random-quote-machine/src/components/QuoteBox/NewQuoteButton/index.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import NewQuoteButton from './NewQuoteButton';
-
-const mapStateToProps = ({ status: { isLoading }, nextId }) => ({
-    isLoading,
-    nextId
-});
-
-export default connect(mapStateToProps)(NewQuoteButton);
