d2c2498c812879bd44dbc8852a947807820f476b
=Andrew Steffey <asteffey@gmail.com>
2019-08-18 12:58:42 -0400

    Test 'renders without crashing' on new Root component
    'renders without crashing' failed on App because of missing
    react-redux Provider.  Moved Provider wrapper out of index.js
    to Root component.  Renamed App.test.js to Root.test.js and now
    testing Root.

--- front-end-libraries-projects/build-a-random-quote-machine/src/index.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import store from "./store";
-import { Provider } from 'react-redux'
-import App from './components/App'
 import * as serviceWorker from './serviceWorker';
+import Root from './components/Root';
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Root />,
     document.getElementById('root')
 );
 
