e7b4fd9fc19ab058fee8dc33fcb26a2285dd5187
=Andrew Steffey <asteffey@gmail.com>
2019-08-17 23:13:39 -0400

    Cleanup css/sass

--- front-end-libraries-projects/build-a-random-quote-machine/src/index.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
 import store from "./store";
 import { Provider } from 'react-redux'
 import App from './components/App'
