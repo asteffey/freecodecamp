ed46944503eae6b5a75756422a769b55385eb5be
=Andrew Steffey <asteffey@gmail.com>
2019-08-17 22:12:16 -0400

    Have FETCH_QUOTE cancel a current fetch operation

--- front-end-libraries-projects/build-a-random-quote-machine/src/reducers/index.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import quote from './quote'
 import status from './status'
 import nextId from './nextId'
+import cancel from './cancel';
 
 export default combineReducers({
     quote,
     status,
-    nextId
+    nextId,
+    cancel
 })
