8c542f19f5aa2ba21abc57c89f1240cf5d478fbb
=Andrew Steffey <asteffey@gmail.com>
2019-08-17 22:47:57 -0400

    Open Tweet Dialog in new tab

--- front-end-libraries-projects/build-a-random-quote-machine/src/components/TweetButton.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/components/TweetButton.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default () => (
-    <a id="tweet-quote" href={`https://twitter.com/intent/tweet?url=${window.location.href}`}>Tweet Quote</a>
+    <a id="tweet-quote"
+        href={`https://twitter.com/intent/tweet?url=${window.location.href}`}
+        rel="noopener noreferrer"
+        target='_blank'>
+        Tweet Quote
+    </a>
 );
