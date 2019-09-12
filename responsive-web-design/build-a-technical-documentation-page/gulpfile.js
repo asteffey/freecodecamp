046d65aa61e3565c0d1b119eac36225ef798e221
=Andrew Steffey <asteffey@gmail.com>
2019-09-12 00:13:16 -0400

    Add build-a-technical-documentation-page to lerna

---
+++ responsive-web-design/build-a-technical-documentation-page/gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const minifyHtml = require('gulp-minify-html');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const minifyCss = require('gulp-minify-css');
+
+
+async function clean() {
+    return del.sync('dist');
+}
+
+async function html() {
+    return gulp
+        .src('./app/**/*.html')
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('dist'));
+}
+
+async function js() {
+    return gulp
+        .src('app/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(terser())
+        .pipe(gulp.dest('dist'));
+}
+
+async function css() {
+    return gulp
+        .src('app/**/*.css')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist'));
+}
+
+async function start() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+        startPath: '?fcc-test=true'
+    });
+
+    gulp.watch('./app/**/*.+(html|css|js)').on("change", browserSync.reload);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, js, css));
+
+exports.clean = clean;
+exports.html = html;
+exports.js = js;
+exports.css = css;
+exports.build = build;
+
+exports.start = start;
+exports.default = start;
