b9240ed787a7c8c438a2754d21078edd8db42b89
Andrew Steffey <asteffey@gmail.com>
2019-09-28 16:43:07 -0400

    Quotebox fixes (#17)
    * Make css more responsive
    * Fix quote 920
    * Use Skeleton Loading
    * Add Skeleton loading to Quote component.
    * Remove use of isLoading state, just use undefined if not yet loaded.
    * Use default static width and height for quote-box, estimated to be large enough for all quotes.
    * Remove Loading component.
    * Add Error component, use larger red error icon.

--- front-end-libraries-projects/build-a-random-quote-machine/src/components/QuoteBox/Quote.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/components/QuoteBox/Quote.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+
+const wordLengths = [3, 2, 8, 4, 8, 4, 3, 1, 4, 7, 1, 6, 6, 5, 4, 6, 9, 2, 3, 7, 4, 2, 7, 3, 3, 4, 4, 6, 2, 1, 5, 2, 5, 8, 3, 6, 9, 4, 3, 1, 6, 2, 9, 8, 11, 6];
+const skeletonParagraph = wordLengths.flatMap((w, i) => [<Skeleton key={i} width={w * 9} />, ' ']);
+const SkeletonText = () => (
+    <span>{skeletonParagraph}</span>
+);
 
 const Quote = ({ text, author }) => {
     return (
-        <div>
-            <p id="text">{text}</p>
-            <p id="author">{author}</p>
+        <div id="quote">
+            <p id="text">{text || <SkeletonText />}</p>
+            <p id="author">{author || <Skeleton width={200}/>}</p>
         </div>
     );
 };
