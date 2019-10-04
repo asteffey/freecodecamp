4894c38ba806a7e835bd9e1beff6c58f7e16f666
=Andrew Steffey <asteffey@gmail.com>
2019-10-04 09:58:30 -0400

    Reorganize with MarkdownEditor component
    MarkdownEditor contains all markdown editing.
    
    App loads FCC tests and the MarkdownEditor.

---
+++ front-end-libraries-projects/build-a-markdown-previewer/src/MarkdownEditor/constants.js
@@ -0,0 +1,21 @@
+export const initialMarkdown =
+`# Header 1
+line
+breaks
+are
+working
+
+## Header 2
+[link](https://freecodecamp.org)
+\`\`\`
+for (let i=0; i<Math.pow(2,1000); i++)
+console.log('this will take a really long time to execute, please don\\'t try it...');
+\`\`\`
+This is a list
+- of
+- some
+- **bold**
+- stuff
+- with \`console.log('code')\`
+> "Markdown is a text-to-HTML conversion tool for web writers" - John Gruber, inventor of Markdown.
+![markdown](https://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)`;
