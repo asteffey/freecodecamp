b6f012e2040e3862aa38c1176b1a6cad6bdb131a
=Andrew Steffey <asteffey@gmail.com>
2019-10-04 00:59:31 -0400

    Use @asteffey/react-fcc-test hook to add FCC tests
    Sets drop-down list to random-quote-machine test.
    
    Only displays FCC test suite if 'fcc-test' is present in query string.

--- front-end-libraries-projects/build-a-random-quote-machine/src/components/App.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
 import '../sass/App.scss';
+import React from 'react';
 import QuoteBox from './QuoteBox';
+import { useFccTest, FccTests } from '@asteffey/react-fcc-test';
 
-const App = () => (
-    <main>
-        <QuoteBox />
-    </main>
-);
+const App = () => {
+    useFccTest({
+        fccTest: FccTests.random_quote_machine,
+        queryParam: 'fcc-test'
+    });
+
+    return (
+        <main>
+            <QuoteBox />
+        </main>
+    );
+};
 
 export default App;
