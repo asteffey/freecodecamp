518619fb002c13690d71e068655ad69e04bd3df8
=Andrew Steffey <asteffey@gmail.com>
2019-10-20 17:26:55 -0400

    Refactor SideBySide
    Use media query instead of useDimensions hook

--- front-end-libraries-projects/build-a-markdown-previewer/src/components/SideBySide.js
+++ front-end-libraries-projects/build-a-markdown-previewer/src/components/SideBySide.js
@@ -1,31 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import useDimensions from 'react-use-dimensions';
 
-const SideBySideDiv = styled.div`
+const SideBySide = styled.div`
     display: grid;
-    grid-template-columns: ${({ isWider }) => isWider ? '50% 50%' : '100%'};
-    grid-template-rows: ${({ isWider }) => isWider ? '100%' : '50% 50%'};
-    height: calc(100vh - 52px);    
+    height: calc(100vh - 52px);
+    @media (orientation: portrait) {
+        grid-template-columns: 100%;
+        grid-template-rows: 50%;
+    }
+    @media (orientation: landscape) {
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 100%;
+    }   
 `;
-
-const SideBySide = ({ children }) => {
-    const [ref, { width, height }] = useDimensions();
-    const isWider = width > height;
-
-    return (
-        <SideBySideDiv ref={ref} isWider={isWider}>
-            {children}
-        </SideBySideDiv>
-    );
-};
-
-SideBySide.propTypes = {
-    children: PropTypes.oneOf([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ]).isRequired
-};
 
 export default SideBySide;
