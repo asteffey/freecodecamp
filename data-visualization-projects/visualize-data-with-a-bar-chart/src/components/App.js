eb482c3a37141ba7801905b92a86fb64b6bf8307
=Andrew Steffey <asteffey@gmail.com>
2020-01-30 00:32:44 -0500

    Initial Working Version
    passing all FCC tests

--- data-visualization-projects/visualize-data-with-a-bar-chart/src/components/App.js
+++ data-visualization-projects/visualize-data-with-a-bar-chart/src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import GdpChart from './GdpChart';
+import { useFccTest, FccTests } from '@asteffey/react-fcc-test';
 
-function App() {
-  return (
-    <div>
-      This Website Is Under Construction!
-    </div>
-  );
-}
+const App = () => {
+    useFccTest({
+        fccTest: FccTests.bar_chart,
+        queryParam: 'fcc-test'
+    });
+
+    return (
+        <main>
+            <GdpChart />
+        </main>
+    );
+};
 
 export default App;
