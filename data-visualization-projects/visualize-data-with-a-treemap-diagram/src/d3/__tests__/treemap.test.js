bc3046b2f5c5ea23234a70cf22a61997a2acaca9
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-06-08 23:16:56 -0400

    feat(treemap): Add legend

--- data-visualization-projects/visualize-data-with-a-treemap-diagram/src/d3/__tests__/treemap.test.js
+++ data-visualization-projects/visualize-data-with-a-treemap-diagram/src/d3/__tests__/treemap.test.js
@@ -50,51 +50,78 @@ beforeEach(() => {
   treemap(fakeData)(svg, width, height)
 })
 
-it('should have <rect> elements with a corresponding class="tile" that represent the data', () => {
-  const tile = document.querySelector('rect.tile')
-  expect(tile).toBeInTheDocument()
-})
-
-it('should be at least 2 different fill colors used for the tiles', () => {
-  const tiles = [...document.querySelectorAll('rect.tile')]
-
-  const colors = new Set()
-  tiles.forEach(tile =>
-    colors.add(tile.style.fill || tile.getAttribute('fill'))
-  )
-
-  expect(colors.size).toBeGreaterThanOrEqual(2)
-})
-
-describe('tile', () => {
-  it('should have the properties data-name, data-category, and data-value containing their corresponding name, category, and value', () => {
-    const tiles = [...document.querySelectorAll('rect.tile')]
-    const items = mapBy(
-      fakeData.children
-        .flatMap(({ children }) => children)
-        .map(item => item),
-      'name')
-
-    tiles.forEach(tile => {
-      const name = tile.getAttribute('data-name')
-      const item = items[name]
-
-      expect(item).not.toBeUndefined()
-      expect(tile.getAttribute('data-value')).toEqual(item.value)
-      expect(tile.getAttribute('data-category')).toEqual(item.category)
-    })
+describe('tiles', () => {
+  it('should have <rect> elements with a corresponding class="tile" that represent the data', () => {
+    const tile = document.querySelector('rect.tile')
+    expect(tile).toBeInTheDocument()
   })
 
-  it('area should correspond to the data-value amount', () => {
-    const allTiles = [...document.querySelectorAll('rect.tile')]
-    const tilesByCategory = groupBy(allTiles, 'category')
+  it('should be at least 2 different fill colors used for the tiles', () => {
+    const tiles = [...document.querySelectorAll('rect.tile')]
 
-    Object.keys(tilesByCategory).forEach(category => {
-      const tiles = tilesByCategory[category]
-      tiles.sort((a, b) => a.getAttribute('data-value') - b.getAttribute('data-value'))
+    const colors = new Set()
+    tiles.forEach(tile =>
+      colors.add(tile.style.fill || tile.getAttribute('fill'))
+    )
 
-      const areas = tiles.map(tile => tile.getAttribute('width') * tile.getAttribute('height'))
-      expect(areas).toEqual([...areas].sort(ascending))
+    expect(colors.size).toBeGreaterThanOrEqual(2)
+  })
+
+  describe('tile', () => {
+    it('should have the properties data-name, data-category, and data-value containing their corresponding name, category, and value', () => {
+      const tiles = [...document.querySelectorAll('rect.tile')]
+      const items = mapBy(
+        fakeData.children
+          .flatMap(({ children }) => children)
+          .map(item => item),
+        'name')
+
+      tiles.forEach(tile => {
+        const name = tile.getAttribute('data-name')
+        const item = items[name]
+
+        expect(item).not.toBeUndefined()
+        expect(tile.getAttribute('data-value')).toEqual(item.value)
+        expect(tile.getAttribute('data-category')).toEqual(item.category)
+      })
     })
+
+    it('area should correspond to the data-value amount', () => {
+      const allTiles = [...document.querySelectorAll('rect.tile')]
+      const tilesByCategory = groupBy(allTiles, 'category')
+
+      Object.keys(tilesByCategory).forEach(category => {
+        const tiles = tilesByCategory[category]
+        tiles.sort((a, b) => a.getAttribute('data-value') - b.getAttribute('data-value'))
+
+        const areas = tiles.map(tile => tile.getAttribute('width') * tile.getAttribute('height'))
+        expect(areas).toEqual([...areas].sort(ascending))
+      })
+    })
+  })
+})
+
+describe('legend', () => {
+  it('should have a legend with a corresponding id="legend"', () => {
+    const legend = document.querySelector('#legend')
+
+    expect(legend).toBeInTheDocument()
+  })
+
+  it('should have rect elements with a corresponding class="legend-item"', () => {
+    const legendItem = document.querySelector('#legend rect.legend-item')
+
+    expect(legendItem).toBeInTheDocument()
+  })
+
+  it('should have at least 2 different fill colors used for the legend', () => {
+    const legendRects = document.querySelectorAll('#legend rect.legend-item')
+
+    const colors = new Set()
+    legendRects.forEach(county =>
+      colors.add(county.style.fill || county.getAttribute('fill'))
+    )
+
+    expect(colors.size).toBeGreaterThanOrEqual(2)
   })
 })
--- data-visualization-projects/visualize-data-with-a-treemap-diagram/src/d3/treemap.js
+++ data-visualization-projects/visualize-data-with-a-treemap-diagram/src/d3/treemap.js
@@ -1,9 +1,7 @@
 import d3plus from './d3plus'
 import darkColorScheme19 from './darkColorScheme19'
 
-const chartPadding = { top: 5, right: 5, bottom: 30, left: 5 }
-
-const colorScale = d3plus.scaleOrdinal(darkColorScheme19)
+const chartPadding = { top: 5, right: 5, bottom: 85, left: 5 }
 
 const treemapChart = (rawData) => (svg, svgWidth, svgHeight) => {
   const { chart, width, height } = paddedChart(svg, svgWidth, svgHeight, chartPadding)
@@ -16,6 +14,7 @@ const treemapChart = (rawData) => (svg, svgWidth, svgHeight) => {
 
   chart
     .call(appendTiles, data)
+    .call(appendLegend, data)
 }
 
 function paddedChart (svg, svgWidth, svgHeight, { left, top, right, bottom }) {
@@ -36,7 +35,7 @@ function paddedChart (svg, svgWidth, svgHeight, { left, top, right, bottom }) {
 }
 
 function parse (rawData, width, height) {
-  return toTreemap(rawData, width, height)
+  const data = toTreemap(rawData, width, height)
     .map(({ x0, x1, y0, y1, ...other }) => ({
       ...other,
       x: x0,
@@ -44,6 +43,17 @@ function parse (rawData, width, height) {
       width: x1 - x0,
       height: y1 - y0
     }))
+
+  const colorScale = d3plus.scaleOrdinal()
+    .domain([...new Set(data.map(({ data: { category } }) => category))])
+    .range(darkColorScheme19)
+
+  return {
+    data,
+    colorScale,
+    width,
+    height
+  }
 }
 
 function toTreemap (rawData, width, height) {
@@ -57,7 +67,7 @@ function toTreemap (rawData, width, height) {
   return layout(hierarchy).leaves()
 }
 
-function appendTiles (chart, data) {
+function appendTiles (chart, { data, colorScale }) {
   const maxArea = d3plus.max(data, ({ width, height }) => width * height)
   const textScaleFactor = 0.3
   function scaleText (width, height) {
@@ -69,7 +79,7 @@ function appendTiles (chart, data) {
 
   chart.appendForEach('g', data)
     .attrs(({ x, y }) => ({
-      class: 'tile',
+      class: 'tileParent',
       transform: `translate(${x}, ${y})`
     }))
     .call(g => {
@@ -92,6 +102,31 @@ function appendTiles (chart, data) {
         .call(d3plus.textwrap().bounds({ width, height }))
       g.select('div')
         .attr('style', `font-size: ${scaleText(width, height) * 20}px`)
+    })
+}
+
+function appendLegend (chart, { colorScale, width, height }) {
+  const padding = 10
+  const shapeWidth = (width - 2 * padding) / colorScale.domain().length - padding
+
+  chart.append('g')
+    .attrs({
+      id: 'legend',
+      transform: `translate(${padding}, ${height + 2 * padding})`
+    })
+    .style('font-size', '12px')
+    .call(d3plus.legendColor()
+      .orient('horizontal')
+      .shape('rect')
+      .shapePadding(padding)
+      .shapeHeight(15)
+      .shapeWidth(shapeWidth)
+      .labelWrap(shapeWidth)
+      .scale(colorScale)
+    )
+    .call(g => {
+      g.selectAll('rect')
+        .attr('class', 'legend-item')
     })
 }
 
