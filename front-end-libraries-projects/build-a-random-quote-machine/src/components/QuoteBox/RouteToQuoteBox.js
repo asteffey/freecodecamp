d042139158a30c5fe3483f62323586a91da989a3
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-07-15 21:16:01 -0400

    refactor: prepare to split apart monorepo"
    Change "build-a" and "visualize-data-with-a-" prefixes to just "fcc-"
    to denote new repo naming scheme.

--- front-end-libraries-projects/build-a-random-quote-machine/src/components/QuoteBox/RouteToQuoteBox.js
+++ front-end-libraries-projects/build-a-random-quote-machine/src/components/QuoteBox/RouteToQuoteBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import RedirectToRandomQuote from './RedirectToRandomQuote';
 import QuoteBox from './QuoteBox';
-//basename={'/freecodecamp/build-a-random-quote-machine'}
+
 const RouteToQuoteBox = () => (
     <Router>
         <Switch>
