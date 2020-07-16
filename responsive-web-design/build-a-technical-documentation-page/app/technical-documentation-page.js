cb144690023a0722011cc82cdfa1b4b416a7ea28
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-07-15 21:28:34 -0400

    refactor: replace responsive-web-design projects with submodules

--- responsive-web-design/build-a-technical-documentation-page/app/technical-documentation-page.js
+++
@@ -1,4 +0,0 @@
-const searchParams = new URLSearchParams(window.location.search);
-if (searchParams.get('fcc-test') === 'true') {
-    document.querySelector("nav>header").classList.add("with-fcc-test");
-}
