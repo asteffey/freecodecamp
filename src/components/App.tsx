c5a60f5e16fe8c1a98cf2a1d3d0d13a7524b771e
=Andrew Steffey <23619800+asteffey@users.noreply.github.com>
2020-07-17 04:44:14 -0400

    revert: remove welcome section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import Projects from './Projects'
 function App () {
   return (
     <>
-      <h1>Andrew Steffey&apos;s freeCodeCamp Portfolio</h1>
+      <h1>Andrew Steffey&apos;s freeCodeCamp Projects</h1>
       <Projects/>
     </>
   )
